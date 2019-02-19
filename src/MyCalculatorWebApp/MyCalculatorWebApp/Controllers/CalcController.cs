81f3c488ecc9e7d43d5b9943404e95ab28a36988
Luis Filipe Costa <lfilipecosta3@gmail.com>
2019-02-19 17:45:44 +0000

    Commenting the code for the "ModelClassWebApp.Service.xml" file

--- src/MyCalculatorWebApp/MyCalculatorWebApp/Controllers/CalcController.cs
+++ src/MyCalculatorWebApp/MyCalculatorWebApp/Controllers/CalcController.cs
@@ -9,13 +9,23 @@
     [ApiController]
     public class CalcController : ControllerBase
     {
-        // GET api/values
+        /// <summary>
+        /// Retrieves the list of values
+        /// </summary>
+        /// <returns>200 OK</returns>
+        // GET api/calc
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
+        /// <summary>
+        /// Add two number and return the result
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns>200 OK; 404 Bad Request</returns>
         // POST api/values
         [HttpPost("add/{n1}/{n2}")]
         public ActionResult<int> PostNumbers(int n1, int n2)
