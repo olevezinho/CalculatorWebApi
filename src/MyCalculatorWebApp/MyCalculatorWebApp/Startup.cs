55e39cb20bedf05c4ac5b6dd96234668400c50c2
Luis Filipe Costa <lfilipecosta3@gmail.com>
2019-03-07 18:35:47 +0000

    Changes to the files: - Dockerfile - Startup.cs (line 50) - ModelClassWebApp.Service.csproj Added the files: - index.html - deploy.yaml

--- src/MyCalculatorWebApp/MyCalculatorWebApp/Startup.cs
+++ src/MyCalculatorWebApp/MyCalculatorWebApp/Startup.cs
@@ -47,11 +47,11 @@
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            //app.UseHttpsRedirection();
+            app.UseMvc( );
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
