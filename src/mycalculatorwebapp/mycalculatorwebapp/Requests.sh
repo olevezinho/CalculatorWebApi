06c73bc6aba930bbc1d56e2938e0062179e4b9d5
Luis Filipe Costa <lfilipecosta3@gmail.com>
2019-06-22 16:42:07 +0100

    Removing requests.sh file

--- src/mycalculatorwebapp/mycalculatorwebapp/Requests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# requests.sh
-
-curl -X GET "http://localhost:8123/api/Calc" -H "accept: application/json"
-curl -i -d -XPOST "http://localhost:8123/api/Calc/add/1/1" -H "accept: application/json"
